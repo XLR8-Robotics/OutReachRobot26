690e5b563c845dfd7277982ee91719abf2f94ae8
Scotty22386 <55546370+Scotty22386@users.noreply.github.com>
2026-02-02 12:51:55 -0500

    Updates

---
+++ TeamCode/src/main/java/Teleops/BasicDriveTrainTeleop.java
@@ -0,0 +1,25 @@
+package Teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Basic Drive Train", group = "Basic")
+public class BasicDriveTrainTeleop extends OpMode {
+
+    @Override
+    public void init()
+    {
+    }
+
+    @Override
+    public void start()
+    {
+
+    }
+
+    @Override
+    public void loop()
+    {
+    }
+
+}
